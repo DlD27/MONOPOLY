3c88a6a8434d44e0af17cb90c98f512f4ded2ecd
DlD27 <duld12271227@gmail.com>
2025-01-19 17:33:22 +1100

    Update parsing files of board and rolls

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-# main.py
-
+import sys
 from game_logic import play_game
 from rolls import load_rolls
 from board import load_board
 from player import Player
 
-def main():
+def main(board_file, rolls_file):
 
     # Load the board and dice rolls
-    board = load_board("/Users/esmeralda/Downloads/new_coding_test/board.json")
-    rolls = load_rolls("/Users/esmeralda/Downloads/new_coding_test/rolls_2.json")
+    board = load_board(board_file)
+    rolls = load_rolls(rolls_file)
 
     # Initialise players
     players = [
@@ -19,8 +18,12 @@ def main():
         Player("Sweedal", board)
     ]
 
+    # Simulate the game
     if board and rolls:
         play_game(board, rolls, players)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 3:
+        print(f"Expecting: python main.py <board_file> <rolls_file>")
+    else:
+        main(sys.argv[1], sys.argv[2])
